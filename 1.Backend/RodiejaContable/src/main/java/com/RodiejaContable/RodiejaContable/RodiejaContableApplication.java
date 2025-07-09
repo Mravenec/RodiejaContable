ae4b124dc35a96737c5a03c033ead31f30dc78c2
mravenec <kcamposgo90@gmail.com>
2025-07-09 12:32:36 -0600

    Funcional mvn spring-boot:run

--- 1.Backend/RodiejaContable/src/main/java/com/RodiejaContable/RodiejaContable/RodiejaContableApplication.java
+++ 1.Backend/RodiejaContable/src/main/java/com/RodiejaContable/RodiejaContable/RodiejaContableApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RodiejacontableApplication {
+public class RodiejaContableApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RodiejacontableApplication.class, args);
+		SpringApplication.run(RodiejaContableApplication.class, args);
 	}
 
 }
